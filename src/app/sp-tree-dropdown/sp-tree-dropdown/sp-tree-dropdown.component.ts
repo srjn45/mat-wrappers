ef8cb5a76f1ed1e96deb2e627ff4cf7431039d75
srjn45 <srajanpathak45@gmail.com>
2018-02-24 14:48:17 +0530

    added demo tree code

--- src/app/sp-tree-dropdown/sp-tree-dropdown/sp-tree-dropdown.component.ts
+++ src/app/sp-tree-dropdown/sp-tree-dropdown/sp-tree-dropdown.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeviewItem } from 'ngx-treeview';
 
 @Component({
-  selector: 'app-sp-tree-dropdown',
+  selector: 'sp-tree-dropdown',
   templateUrl: './sp-tree-dropdown.component.html',
   styleUrls: ['./sp-tree-dropdown.component.css']
 })
 export class SpTreeDropdownComponent implements OnInit {
+
+  public config = {
+    hasAllCheckBox: true,
+    hasFilter: false,
+    hasCollapseExpand: false,
+    decoupleChildFromParent: false,
+    maxHeight: 500
+  }
+  items = [itCategory];
 
   constructor() { }
 
@@ -13,3 +23,28 @@ export class SpTreeDropdownComponent implements OnInit {
   }
 
 }
+const itCategory = new TreeviewItem({
+  text: 'IT', value: 9, children: [
+    {
+      text: 'Programming', value: 91, children: [{
+        text: 'Frontend', value: 911, children: [
+          { text: 'Angular 1', value: 9111 },
+          { text: 'Angular 2', value: 9112 },
+          { text: 'ReactJS', value: 9113 }
+        ]
+      }, {
+        text: 'Backend', value: 912, children: [
+          { text: 'C#', value: 9121 },
+          { text: 'Java', value: 9122 },
+          { text: 'Python', value: 9123, checked: false }
+        ]
+      }]
+    },
+    {
+      text: 'Networking', value: 92, children: [
+        { text: 'Internet', value: 921 },
+        { text: 'Security', value: 922 }
+      ]
+    }
+  ]
+});
