643e717eb4bdeecadeee33e731ed665b05b7ed55
srjn45 <srajanpathak45@gmail.com>
2018-02-24 14:29:09 +0530

    import TreeViewModule

--- src/app/sp-tree-dropdown/sp-tree-dropdown.module.ts
+++ src/app/sp-tree-dropdown/sp-tree-dropdown.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpTreeDropdownComponent } from './sp-tree-dropdown/sp-tree-dropdown.component';
 
+import { TreeviewModule } from "ngx-treeview";
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TreeviewModule.forRoot()
   ],
   declarations: [SpTreeDropdownComponent],
   exports: [
