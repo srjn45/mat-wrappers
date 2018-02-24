3513dc25cad4e51e1f1dfb89e2c01dbaf0cae9d7
srjn45 <srajanpathak45@gmail.com>
2018-02-24 14:47:49 +0530

    model class fro tree node

---
+++ src/app/sp-tree-dropdown/node.ts
@@ -0,0 +1,12 @@
+export class Node {
+
+    constructor(
+        text: string,
+        value: any,
+        children: Node[],
+        checked: boolean = false,
+        collapsed: boolean = true,
+        disabled: boolean = false
+    ) { }
+
+}
