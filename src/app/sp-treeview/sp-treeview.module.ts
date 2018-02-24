dd07593402d854874cc2a4cf3b160995e91c2ede
srjn45 <srajanpathak45@gmail.com>
2018-02-24 18:54:50 +0530

    custom treeview module

---
+++ src/app/sp-treeview/sp-treeview.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpTreeviewItemComponent } from './sp-treeview-item/sp-treeview-item.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SpTreeviewItemComponent]
+})
+export class SpTreeviewModule { }
