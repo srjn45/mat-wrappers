1c1a845c742fea22f6d20794fc4880bd2f49ab79
srjn45 <srajanpathak45@gmail.com>
2018-02-26 01:53:47 +0530

    takes nodes array and config object as input and emmits change event combined radioSelect and checkboxSelect event into change

--- src/app/sp-treeview/sp-treeview/sp-treeview.component.ts
+++ src/app/sp-treeview/sp-treeview/sp-treeview.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, QueryList, Output, EventEmitter } from '@angular/core';
+import { Node } from '../model/node';
+import { Config, SELECT_CHECKBOX, SELECT_RADIO } from '../model/config';
+import { SpTreeviewNodeComponent } from '../sp-treeview-node/sp-treeview-node.component';
 
 @Component({
   selector: 'sp-treeview',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpTreeviewComponent implements OnInit {
 
+  @Input() nodes: Node[];
+  @Input() config: Config = new Config();
+
+  @Output() change: EventEmitter<any> = new EventEmitter<any>();
+
+  @ViewChildren('tree') trees: QueryList<SpTreeviewNodeComponent>;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  onChange(event) {
+    if (this.config.select == SELECT_CHECKBOX) {
+      let values = [];
+      this.trees.forEach(t => {
+        t.getCheckedValues().forEach(v => values.push(v))
+      });
+      this.change.emit(values);
+    } else if (this.config.select == SELECT_RADIO) {
+      this.change.emit(event);
+    }
+  }
 }
