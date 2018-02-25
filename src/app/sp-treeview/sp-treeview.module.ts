9f6a324960d286fe217ab334f9bcb4f9d59eed17
srjn45 <srajanpathak45@gmail.com>
2018-02-25 22:12:28 +0530

    export SpTreeviewNodeComponent import Mat dependency

--- src/app/sp-treeview/sp-treeview.module.ts
+++ src/app/sp-treeview/sp-treeview.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpTreeviewNodeComponent } from './sp-treeview-node/sp-treeview-node.component';
 
+import { MatIconModule, MatRadioModule, MatCheckboxModule, MatButtonModule } from "@angular/material";
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   declarations: [
     SpTreeviewNodeComponent
-  ]
+  ],
+  exports: [SpTreeviewNodeComponent]
 })
 export class SpTreeviewModule { }
